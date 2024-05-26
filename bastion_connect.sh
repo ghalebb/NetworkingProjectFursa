dd2c60ad7b5de5a8383573861bfb824042c6d9f3
ghalebb <ghaleb.butto99@gmail.com>
2024-05-26 15:38:47 +0300

    bastion with rotation

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -11,24 +11,32 @@ if [ -z "$1" ]; then
   exit 5
 fi
 
+
 PUBLIC_IP=$1
 PRIVATE_IP=$2
 COMMAND=$3
+MACHINE_PRIVATE_KEY="/home/ubuntu/key.pem"
 
 
-connect_to_public_instance() {
-  ssh -i "$KEY_PATH" ubuntu@"$PUBLIC_IP"
+if ! ssh -i "$KEY_PATH" ubuntu@"$PUBLIC_IP" "test -f $NEW_KEY_PATH_FILE"; then
+    echo "Failed to find the new key path file on the bastion host"
+    exit 1
+fi
+
+function connect_to_public_instance() {
+    ssh -t -i "$KEY_PATH" ubuntu@"PUBLIC_IP"
 }
 
-connect_to_private_instance() {
-  ssh -i "$KEY_PATH" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ubuntu@$PUBLIC_IP" ubuntu@"$PRIVATE_IP"
- #  ssh -i "$KEY_PATH" -J ubuntu@"$PUBLIC_IP" ubuntu@"$PRIVATE_IP"
+function connect_to_private_instance() {
+      ssh -i "$MACHINE_PRIVATE_KEY" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ubuntu@$PUBLIC_IP" ubuntu@"$PRIVATE_IP"
+    #ssh -t -i "$KEY_PATH" ubuntu@"$PUBLIC_IP" "ssh -t -i $MACHINE_PRIVATE_KEY ubuntu@$PRIVATE_IP"
 }
 
-run_command_on_private_instance() {
-  ssh -i "$KEY_PATH" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ubuntu@$PUBLIC_IP" ubuntu@"$PRIVATE_IP" "$COMMAND"
-#  ssh -i "$KEY_PATH" -J ubuntu@"$PUBLIC_IP" ubuntu@"$PRIVATE_IP" "$COMMAND"
+function run_command_private() {
+	ssh -i "$MACHINE_PRIVATE_KEY" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ubuntu@$PUBLIC_IP" ubuntu@"$PRIVATE_IP" "$COMMAND"    
+#ssh -t -i "$KEY_PATH" ubuntu@"$PUBLIC_IP" "ssh -t -i $MACHINE_PRIVATE_KEY ubuntu@$PRIVATE_IP '$COMMAND'"
 }
+
 
 if [ -z "$PRIVATE_IP" ]; then
   connect_to_public_instance
@@ -37,4 +45,3 @@ elif [ -z "$COMMAND" ]; then
 else
   run_command_on_private_instance
 fi
-
